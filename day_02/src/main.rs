0c617db349f39f1a357ef5323b7deb3e48ca4f3e
pawndev <coquelet.c@gmail.com>
2018-12-04 14:09:08 +0100

    Add poart 1 of day 2

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,3 +1,43 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::collections::BTreeMap;
+
+fn open_file() -> String {
+  let f = File::open("files/input.txt");
+  let mut contents = String::new();
+  f.unwrap().read_to_string(&mut contents).expect("Something went wrong while readin the file");
+
+  contents
+}
+
+fn part_one() {
+  let contents = open_file();
+  let mut twos = 0;
+  let mut threes = 0;
+
+  for line in contents.lines() {
+    let mut occ = BTreeMap::new();
+    let mut already_two = false;
+    let mut already_three = false;
+
+    //line.chars().map(|l| *occ.entry(l).or_insert(0) += 1);
+    for l in line.chars() {
+      *occ.entry(l).or_insert(0) += 1
+    }
+    //println!("{:?}", line.chars());
+    for (_k, v) in &occ {
+      match v {
+        3 => if already_three {()} else {threes += 1; already_three = true},
+        2 => if already_two {()} else {twos += 1; already_two = true},
+        _ => ()
+      }
+    }
+    //println!("{:?}", occ);
+    println!("twos: {:?}, threes: {:?}", twos, threes);
+  }
+  println!("twos: {:?}, threes: {:?}, twos*threes={:?}", twos, threes, twos * threes);
+}
+
 fn main() {
-    println!("Hello, world!");
+  part_one();
 }
